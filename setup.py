bd3b82715d30e5636967452e5023eb00d58251b9
Bryant Luu <bryant@numer.ai>
2018-12-09 16:48:40 -0800

    finish set 1 problem 7

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
         "awscli", "boto3", "flake8", "joblib", "pandas>=0.23.4", "psycopg2",
         "pylint==1.7.4", "python-dotenv", "requests", "progressbar", "sklearn",
         "tqdm", "ujson", "web3", "sendgrid==5.6.0", "pypd", "premailer",
-        "jinja2", "botocore>=1.12.38", "scipy"
+        "jinja2", "botocore>=1.12.38", "scipy", "pycrypto"
     ],
     packages=["crypto"])
